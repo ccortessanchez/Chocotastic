d1991c7f1a32b813e454b5cf42801857d132e451
Carlos Cortés Sánchez <ccortes@gmv.es>
2017-07-05 16:37:08 +0200

    Reactive chocolates table view and tap action

--- Chocotastic/ChocolatesOfTheWorldViewController.swift
+++ Chocotastic/ChocolatesOfTheWorldViewController.swift
@@ -28,7 +28,7 @@ class ChocolatesOfTheWorldViewController: UIViewController {
   
   @IBOutlet private var cartButton: UIBarButtonItem!
   @IBOutlet private var tableView: UITableView!
-  let europeanChocolates = Chocolate.ofEurope
+  let europeanChocolates = Observable.just(Chocolate.ofEurope)
   let disposeBag = DisposeBag()
   
   //MARK: View Lifecycle
@@ -36,10 +36,9 @@ class ChocolatesOfTheWorldViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Chocolate!!!"
-
-    tableView.dataSource = self
-    tableView.delegate = self
     setupCartObserver()
+    setupCellConfiguration()
+    setupCellTapHandling()
   }
   
   //MARK: Rx Setup
@@ -49,51 +48,47 @@ class ChocolatesOfTheWorldViewController: UIViewController {
     // We call subscribe(onNext:) in order to find out about changes to the Observable's values subscribe(onNext:) accepts a closure that will be executed every time a value changes
     // Incoming parameter to the closure is the new value of Observable, we'll keep getting notifications until unsubscribing or subscription is disposed
     // We get back an Observer conforming Disposable
-    // Add Observer to disposebag to ensure subscription is disposed of when unsubscribing object is deallocated
+    // Add Observer to disposeBag to ensure subscription is disposed of when unsubscribing object is deallocated
     ShoppingCart.sharedCart.chocolates.asObservable()
       .subscribe(onNext: { chocolates in
       self.cartButton.title = "\(chocolates.count) \u{1f36b}"
     })
     .addDisposableTo(disposeBag)
   }
-}
-
-// MARK: - Table view data source
-extension ChocolatesOfTheWorldViewController: UITableViewDataSource {
-
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
   
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return europeanChocolates.count
-  }
-  
-  func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    return false
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = tableView.dequeueReusableCell(withIdentifier: ChocolateCell.Identifier, for: indexPath) as? ChocolateCell else {
-      //Something went wrong with the identifier.
-      return UITableViewCell()
+  private func setupCellConfiguration() {
+    // We call bindTo() to associate europeanChocolates observable with the code that should get executed for each row in tableView
+    // Calling rx we are able to access RxCocoa extensions for the class you call it on
+    // We call Rx method items(). This allows Rx framework to call the dequeueing methods that would be called if the table had its original delegates
+    // We pass in a block to be executed for each new item. We get back information about the row, chocolate at that row and cell
+    // Take the disposable returned by bindTo() and add it to disposeBag
+    europeanChocolates.bindTo(tableView
+      .rx
+      .items(cellIdentifier: ChocolateCell.Identifier, cellType: ChocolateCell.self)) {
+        row, chocolate, cell in
+      cell.configureWithChocolate(chocolate: chocolate)
     }
-    
-    let chocolate = europeanChocolates[indexPath.row]
-    cell.configureWithChocolate(chocolate: chocolate)
-    
-    return cell
+      .addDisposableTo(disposeBag)
   }
-}
-
-// MARK: - Table view delegate
-extension ChocolatesOfTheWorldViewController: UITableViewDelegate {
-
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    tableView.deselectRow(at: indexPath, animated: true)
-    
-    let chocolate = europeanChocolates[indexPath.row]
-    ShoppingCart.sharedCart.chocolates.value.append(chocolate)
+  
+  private func setupCellTapHandling() {
+    // Call tableView reactive extension modelSelected(), passing the Chocolate model to get the proper type of item back. Returns an Observable
+    // Call subscribe(onNext:), passing a closure of what should be done very time a model is selected
+    // Add selected chocolate to the cart
+    // Make sure tapped row is deselected
+    // subscribe(onNext:) returns a Disposable. Add the disposable to disposeBag
+    tableView
+      .rx
+      .modelSelected(Chocolate.self)
+      .subscribe(onNext: {
+        chocolate in
+        ShoppingCart.sharedCart.chocolates.value.append(chocolate)
+        
+        if let selectedRowIndexPath = self.tableView.indexPathForSelectedRow {
+          self.tableView.deselectRow(at: selectedRowIndexPath, animated: true)
+        }
+      })
+      .addDisposableTo(disposeBag)
   }
 }
 
